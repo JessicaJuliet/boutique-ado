224abd0d010e4ec62fb7bc7f3a0df88c678bda7c
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-08-24 20:21:19 +0000

    Complete webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -22,14 +22,14 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
-
 
     def update_total(self):
         """
@@ -44,7 +44,6 @@ class Order(models.Model):
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
-
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the order number
@@ -53,7 +52,6 @@ class Order(models.Model):
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
-
 
     def __str__(self):
         return self.order_number
@@ -66,7 +64,6 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
-
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the lineitem total
@@ -74,7 +71,6 @@ class OrderLineItem(models.Model):
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
-
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
